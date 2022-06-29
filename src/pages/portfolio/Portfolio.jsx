dc361ebbc5db3e9402e8426d50890c3e43f598b6
RubenBaader <rubenbaader@gmail.com>
2022-06-29 20:29:50 +0200

    Carousel hacking - looking to wrap items offscreen

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Carousel.css'
 
-export const CarouselItem = ({ children, width }) => {
+export const CarouselItem = ({ children, width, id }) => {
     return (
-        <div className="carousel-item" style={ {width: width} }>
+        <div className="carousel-item" id={ id } style={ {width: width} }>
             {children}
         </div>
     );
@@ -11,29 +11,62 @@ export const CarouselItem = ({ children, width }) => {
 
 const Carousel = ({ children }) => {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [paused, setPaused] = useState(false);
+
     const updateIndex = (newIndex) => {
-        if (newIndex < 0) {
-            newIndex = 0;
-        } else if (newIndex >= React.Children.count(children)) {
+        if (newIndex < 0) { // overflow left
+
             newIndex = React.Children.count(children) - 1;
+        } else if (newIndex >= React.Children.count(children)) { // overflow right
+            /* children.push(children[0])
+            children.shift() */
+            newIndex = 0;
         }
+        console.log(children)
         setActiveIndex(newIndex);
     };
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (!paused) {
+            updateIndex(activeIndex + 1);
+            }
+        }, 3000)
+        
+        return () => {
+            if (interval) {clearInterval(interval);}
+        };
+    });
 
     return (
-        <div className="carousel">
+        <div 
+        className="carousel"
+        onMouseEnter={ () => setPaused(true) }
+        onMouseLeave={ () => setPaused(false) }
+        >
             <div 
             className="inner" 
-            style={ {transform: `translateX(-${activeIndex * 100}%)`} }>
+            style={ {transform: `translateX(-${activeIndex * 30}%)`} }>
                 {React.Children.map(children, (child, index) => {
-                    return React.cloneElement(child, { width: "100%" } )
+                    console.log(children.length);
+                    return React.cloneElement(child, { width: "30%", id: index } )
                 })};
             </div>
             <div className="indicators">
                 <button onClick={() => {updateIndex(activeIndex - 1);}}>
                     Prev
                 </button>
+
+                {React.Children.map(children, (child, index) => {
+                    return (
+                        <button 
+                            className={`${index === activeIndex ? 'active' : ''}`}
+                            onClick={() => {updateIndex(index);}}>
+                                {index + 1}
+                        </button>
+                    );
+                })}
+
                 <button onClick={() => {updateIndex(activeIndex + 1);}}>
                     Next
                 </button>

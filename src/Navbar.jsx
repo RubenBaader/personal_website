0f3e4424ae858f404fb1fc5bf870297be6f8ca7c
RubenBaader <rubenbaader@gmail.com>
2022-09-04 13:58:29 +0200

    Navbar css refactored to standalone stylesheet

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 // import './App.css';
 
+import './Navbar.css';
+
 function Navbar () {
     return(
         <nav className="nav">

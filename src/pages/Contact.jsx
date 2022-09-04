1768d87c6a7483a1310d8093784e8077c89166bd
RubenBaader <rubenbaader@gmail.com>
2022-09-04 13:14:21 +0200

    Finalized contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,28 +2,44 @@ import React from "react";
 import "./Contact.css"
 
 function Contact() {
+    const email = 'info@example.com';
+    const work_email = 'info+work@example.com';
+    const project_email = 'info+project@example.com';
+
     return (
         <main className="page-wrapper">
             <h1>Contact information</h1>
             <p className="main-text">Here you may find how to contact me.</p>
             <div className="grid-wrapper">
-                <section className="main-text">
-                    For information on my availability for full time work, please send me an email at
+                <section>
+                    <h2>Work</h2>
+                    <hr />
+                    <p className="describe-text">
+                        For information on my availability for full time work, please send me an email at <a className="describe-text" href={`mailto: ${work_email}`}>{work_email}</a>
+                    </p>
                 </section>
                 <div className="mail-button-wrapper">
-                    <a className="main-text mail-button" href="mailto:">email@example.com</a>
+                    <a className="button-text mail-button" href={`mailto: ${work_email}`}>{work_email}</a>
                 </div>
-                <section className="main-text">
-                    If you have a limited project which you think might be of interest to me, feel free to email at
+                <section>
+                    <h2>Project</h2>
+                    <hr />
+                    <p className="describe-text">
+                        If you have a limited project which you think might be of interest to me, feel free to email at <a className="describe-text" href={`mailto: ${project_email}`}>{project_email}</a>
+                    </p>
                 </section>
                 <div className="mail-button-wrapper">
-                    <a className="main-text mail-button" href="mailto:">email+1@example.com</a>
+                    <a className="button-text mail-button" href={`mailto: ${project_email}`}>{project_email}</a>
                 </div>
-                <section className="main-text">
-                    For other questions, mail me at
+                <section>
+                    <h2>Other</h2>
+                    <hr />
+                    <p className="describe-text">
+                        For other questions, mail me at <a className="describe-text" href={`mailto: ${email}`}>{email}</a>
+                    </p>
                 </section>
                 <div className="mail-button-wrapper">
-                    <a className="main-text mail-button" href="mailto:">info@example.com</a>
+                    <a className="button-text mail-button" href={`mailto: ${email}`}>{email}</a>
                 </div>
             </div>
         </main>
